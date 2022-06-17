6b794102a2a70c905dfb838a95c53fe2bbe27b69
AfonsoCunha22 <64204008+AfonsoCunha22@users.noreply.github.com>
2022-06-17 15:16:22 +0100

    Update streamlit_app.py
    Added menu and dishes

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
+streamlit.header('Lunch menu')
+streamlit.text('Pão com Chouriço')
+streamlit.text('Bacalhau à Brás')
+streamlit.text('Xacuti')
