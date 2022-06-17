4e49ede0561210f4dc791cb20fa0a42a180d9859
AfonsoCunha22 <64204008+AfonsoCunha22@users.noreply.github.com>
2022-06-17 16:07:01 +0100

    Update streamlit_app.py
    Changed the name of the variable of fruits to avoid typos

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,8 @@
 import streamlit
 import pandas
 
-my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-my_fruit_list = my_fruit_list.set_index('Fruit')
+my_fruits_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruits_list = my_fruits_list.set_index('Fruit')
 
 streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Lunch menu')
@@ -13,6 +13,6 @@ streamlit.text('🐔 Xacuti')
 
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
-fruits_select = streamlit.multiselect('Pick some fruits:', list(my_fruit_list.index), ['Avocado', 'Strawberries'])
+fruits_select = streamlit.multiselect('Pick some fruits:', list(my_fruits_list.index), ['Avocado', 'Strawberries'])
 my_fruits_to_show = my_fruits_list.loc[fruits_select]
 streamlit.dataframe(my_fruits_to_show)
